e20f068df3f4152daf1fa9e46b8c3fbbcc057046
marcodavidd020 <marcodaviddtc@gmail.com>
2025-06-26 01:35:55 -0400

    chore(database): Remove obsolete migration files for pqrsona table
    - 🗑️ Deleted the migration files for creating and renaming the 'pqrsona' table, as they are no longer needed in the database structure.
    - 🔄 This cleanup aligns with recent changes to the database schema and improves maintainability.

--- database/migrations/2025_06_26_053226_create_personas_table.php
+++ database/migrations/2025_06_26_053226_create_personas_table.php
@@ -11,7 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::rename('pqrsona', 'ppersona');
+        Schema::create('personas', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::rename('ppersona', 'pqrsona');
+        Schema::dropIfExists('personas');
     }
 };
